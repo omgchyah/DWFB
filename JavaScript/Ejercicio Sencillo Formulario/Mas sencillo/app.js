93847cb76011639c51213a6a5a951483269ca33e
Rossana Liendo <rossana.liendo@gmail.com>
2025-07-14 09:20:08 +0200

    Working on the DOM exercises

---
+++ JavaScript/Ejercicio Sencillo Formulario/Mas sencillo/app.js
@@ -0,0 +1,102 @@
+//declara y asignar variables del HTML
+const artNomtInput = document.getElementById("nombre");
+const artDesInput = document.getElementById("descripcion");
+const artPreInput = document.getElementById("precio");
+const artCanInput = document.getElementById("cantidad");
+const artCatInput = document.getElementById("categoria");
+const artSubmit = document.querySelector("form button");
+
+//tabla
+const artTablaContenedor = document.getElementById("articulos-tabla");
+const artTablaCuerpo = document.querySelector("#articulos-tabla tbody");
+
+function toggleTablaVisibility() {
+  const hayArticulos = artTablaCuerpo.children.length > 0;
+
+  artTablaContenedor.style.display = hayArticulos ? "table" : "none";
+}
+
+function agregarArticulo(nom, des, pre, can, cat) {
+  //create elements
+  const artNuevaFila = document.createElement("tr");
+
+  const artNombre = document.createElement("td");
+  const artDescripcion = document.createElement("td");
+  const artPrecio = document.createElement("td");
+  const artCantidad = document.createElement("td");
+  const artCategoria = document.createElement("td");
+  const artAcciones = document.createElement("td");
+
+  //Agregar contenido dentro de  los elementos creados
+  artNombre.textContent = nom;
+  artDescripcion.textContent = des;
+  artPrecio.textContent = parseFloat(pre).toFixed(2);
+  artCantidad.textContent = parseInt(can);
+  artCategoria.textContent = cat;
+
+  //botón de eliminar
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Eliminar";
+  deleteBtn.className = "btn-eliminar";
+
+  deleteBtn.addEventListener("click", () => {
+    artNuevaFila.remove();
+    toggleTablaVisibility();
+  });
+
+  //agregar botón de borrar dentro de acciones
+  artAcciones.appendChild(deleteBtn);
+
+  //Agregar elementos dentro de la fila
+  artNuevaFila.appendChild(artNombre);
+  artNuevaFila.appendChild(artDescripcion);
+  artNuevaFila.appendChild(artPrecio);
+  artNuevaFila.appendChild(artCantidad);
+  artNuevaFila.appendChild(artCategoria);
+  artNuevaFila.appendChild(artAcciones);
+
+  //agregar fila al cuerpo de la tabla
+  artTablaCuerpo.appendChild(artNuevaFila);
+
+
+}
+
+toggleTablaVisibility()
+
+artSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const nom = artNomtInput.value.trim();
+  const des = artDesInput.value.trim();
+  const pre = Number(artPreInput.value.trim());
+  const can = Number(artCanInput.value.trim());
+  const cat = artCatInput.value;
+
+  if (nom.length < 3) {
+    alert("Nombre muy corto.");
+    return;
+  }
+
+  if (des.length < 5) {
+    alert("Descripción muy corta.");
+    return;
+  }
+
+  if (isNaN(pre) || pre <= 0) {
+    alert("Introduzca un precio válido.")
+    return;
+  }
+
+  if (!Number.isInteger(can) || can <= 0) {
+    alert("Introduzca una cantidad mayor a cero.")
+    return;
+  }
+
+  if (!cat) {
+    alert("Debe seleccionar una cantidad.");
+    return;
+  }
+
+  agregarArticulo(nom, des, pre, can, cat);
+  toggleTablaVisibility();
+});
